8159c4bd5eb5e3c4fe2c4d22a00836d45501d3db
isz25 <isidrosaq@gmail.com>
2018-06-24 20:34:43 -0700

    login

---
+++ src/main/resources/templates/controllers/controller.js
@@ -0,0 +1,26 @@
+var app = angular.module('giftShop', ['ngRoute']);
+
+app.config(function($routeProvider){
+    $routeProvider.when('/', {
+        templateUrl: 'login.html'
+     })
+    .when('index',{
+        templateUrl: 'index.html'
+    })
+    .otherwise('index',{
+        redirectTo: '/'
+    });
+});
+
+app.controller('loginController', function($scope, $location){
+    $scope.submit = function(){
+
+        var username = $scope.username;
+        var password = $scope.password;
+        if(username == 'admin' && password == 'admin'){
+            $location.path('/index');
+        }else{
+            alert("Is wrong");
+        }
+    }
+});
